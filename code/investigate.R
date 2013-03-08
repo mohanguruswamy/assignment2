2899438c1812d2c40d41848cc38cee682353acf7
Graham <graham@grahamrp.com>
2013-03-08 16:36:16 +0000

    updated plan.md

--- code/investigate.R
+++ code/investigate.R
@@ -1,5 +1,4 @@
 
 library(tree)
-
-tree1 <- tree(activity ~ ssdTrain$tBodyAcc.mean...X + ssdTrain$tBodyAcc.std...X,data=ssdTrain)
-tree2 <- tree(activity ~ .,data=ssdTrain2)
+tree1 <- tree(activity ~ .,data=ssdTrain)
+summary(tree1)
